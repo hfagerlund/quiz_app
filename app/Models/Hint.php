31269547ad301995293bb4c1496d9e59674f32a0
Heini Fagerlund <heini@bytesofdesign.com>
2023-04-20 19:16:12 -0400

    Define relationship between models

--- app/Models/Hint.php
+++ app/Models/Hint.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Question;
 
 class Hint extends Model
 {
     use HasFactory;
+    
+    protected $fillable = ['question_id', 'hint'];
+    
+    /**
+     * Get the question that owns the hint.
+     */
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
