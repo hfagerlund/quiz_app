25ed8e1deb93db5949d43c1e0f981cd407b803eb
Heini Fagerlund <heini@bytesofdesign.com>
2023-04-21 17:44:24 -0400

    Customize, call seeders
    * Run all seeders using:
    $ ./vendor/bin/sail artisan db:seed

--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -4,14 +4,21 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Question;
 
 class QuestionSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
     public function run(): void
     {
-        //
+        // create 10 questions, with 3 hints per question
+        Question::factory()
+                ->count(10)
+                ->hasHints(3)
+                ->create();
     }
 }
