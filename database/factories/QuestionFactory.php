51ddbff180d4ed2fb53b218e869ffe24890d4de2
Heini Fagerlund <heini@bytesofdesign.com>
2023-04-23 19:43:37 -0400

    Generate factories, define fields for seed data)
    * Automatically generated using:
    $ ./vendor/bin/sail artisan make:factory QuestionFactory --model=Question
    $ ./vendor/bin/sail artisan make:factory HintFactory --model=Hint

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Question;
+use Faker\Generator as Faker;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Question>
+ */
+class QuestionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->text(40),
+            'body' => $this->faker->text(200)
+        ];
+    }
+}
