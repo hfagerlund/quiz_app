6242d70ead5054ded2a89a28fdb834872dc4602e
Heini Fagerlund <heini@bytesofdesign.com>
2023-05-16 01:31:12 -0400

    Add call GraphQL API in App component using axios
    * Displays list of questions (each with all of its hints) when browse to http://0.0.0.0/

--- resources/components/App.vue
+++ resources/components/App.vue
@@ -1,11 +1,51 @@
 <template>
   <h1>Quiz app</h1>
+  <h2>Questions</h2>
+    <div class="post" v-for="questionData in quizDataList" v-bind:key="questionData.id">
+            <h3>Question: {{ questionData.title }}</h3>
+            <p>{{ questionData.body }}</p>
+
+            <h3>Hints</h3>
+        <ul>
+            <li class="just-the-hints" v-for="thehint in questionData.hints" :key="thehint.id">
+              {{ thehint.hint }}
+            </li>
+        </ul>
+        <hr>
+    </div>
 </template>
 
 <script>
 export default {
-  setup() {
-
-   }
-}
+        data() {
+            return {
+                quizDataList: []
+            };
+        },
+        async mounted() {
+            try {
+                var result = await axios({
+                    method: "POST",
+                    url: "http://0.0.0.0/graphql",
+                    data: {
+                        query: `
+                            {
+                                questions {
+                                    id
+                                    title
+                                    body
+                                    hints {
+                                      hint
+                                    }
+                                }
+                            }
+                        `
+                    }
+                });
+                this.quizDataList = result.data.data.questions;
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
 </script>
