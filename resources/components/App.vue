c1fbfb0ca7d0c2a6be30418649a587a2eeb2e8a8
Heini Fagerlund <heini@bytesofdesign.com>
2023-05-31 19:27:34 -0400

    Add buttons to mount (toggle) dynamic components
    * Switch component displayed when browse to homepage (http://0.0.0.0/) to either list of questions with hints, or test component by clicking a button

--- resources/components/App.vue
+++ resources/components/App.vue
@@ -1,22 +1,28 @@
 <template>
   <h1>Quiz app</h1>
-  <div id="app">
-    <component :is="component" />
+
+<div id="app">
+  <button @click="component ='QuestionsList'">QuestionsList</button>
+  <button @click="component ='Testing'">Test component</button>
+
+  <p>*************************************</p>
+  <component :is="component" />
   </div>
+
 </template>
 
 <script>
 import QuestionsList from './QuestionsList.vue';
 import Testing from './Test.vue';
 
-export default {
-  name: 'app',
-  components: {
-    QuestionsList, Testing
-  },
+  export default {
+    name: 'app',
+    components: {
+      QuestionsList, Testing
+    },
   data() {
     return {
-      component: 'QuestionsList',
+      component: null,
     };
   },
 }
