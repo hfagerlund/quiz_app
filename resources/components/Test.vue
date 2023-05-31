d07bd1fd6bba69cc2d21d106aebd4745d71b0e7f
Heini Fagerlund <heini@bytesofdesign.com>
2023-05-31 19:07:41 -0400

    Refactor app component, make dynamic components
    * Refactor: moved functionality from 'App' component to (new) 'QuestionsList' component;
    * add 'Test' component (to demonstrate dynamic components)
      * 'scoped' attribute applies CSS locally to test component only
    * Toggle which dynamic component is displayed (browse to http://0.0.0.0/ to verify) by changing the 'component' variable value in the App.vue default export to the name of the component (ie. to either QuestionsList or Testing)
    * Enables dynamic rendering (mounting, unmounting) of components without using routing

---
+++ resources/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div><h2>Testing</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Testing',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h2 {
+  color:#f00;
+  padding: 1em;
+  text-transform: uppercase;
+}
+</style>
