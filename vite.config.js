73b5adbb785f5e6586c806d8d3f356b84c69ca00
Heini Fagerlund <heini@bytesofdesign.com>
2023-06-01 17:02:30 -0400

    Configure Vitest
    * Add 'test' property in Vite config
    * Add reference to Vitest types using triple slash command (since importing 'defindConfig' from 'vite' itself

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    test: {
+      // ...
+    },  
     plugins: [
         laravel([
             //'resources/css/app.css',
