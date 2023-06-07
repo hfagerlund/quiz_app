2d1ce74229726a08b6da872ab0b352b0ea75ed63
Heini Fagerlund <heini@bytesofdesign.com>
2023-06-07 18:51:41 -0400

    Add default values for new fields
    * Fix/prevent the following error for 'points' and 'points_decreased_by' fields:
    SQLSTATE[HY000]: General error: 1364 Field 'points' doesn't have a default value
    ...when running the following mutation (note: double quotes must be used in GraphiQL):
    mutation {
      createQuestion(title: 'New question', body: 'Lorem ipsum?') {
        id
        title
        body
      }

--- database/migrations/2023_05_31_003040_add_points_decreased_by_to_hints_table.php
+++ database/migrations/2023_05_31_003040_add_points_decreased_by_to_hints_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('hints', function (Blueprint $table) {
-            $table->integer('points_decreased_by');
+            $table->integer('points_decreased_by')->default('1');
         });
     }
 
