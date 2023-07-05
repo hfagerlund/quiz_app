6e2733ea3e2275c7df3dceba4b34001440fe8581
Heini Fagerlund <heini@bytesofdesign.com>
2023-07-04 22:49:54 -0400

    Add (HTML5) drag and drop to Vue3 component
    * works but the following error is displayed in (browser) console (for onDrop):
    Uncaught DOMException: Modifications are not allowed for this document

--- resources/components/Test.vue
+++ resources/components/Test.vue
@@ -1,6 +1,25 @@
 <template>
-  <div><h2>Testing</h2>
+  <div class="container">
+  <div class="start-box">
+    <div
+      id="draggable-1"
+      class="reorder"
+      draggable="true"
+      v-on:dragstart="onDragStart($event)"
+    >
+      <!-- draggable -->
+      Drag this item to the correct drop zone.
+    </div>
   </div>
+
+  <div
+    class="dropzone"
+    v-on:dragover="onDragOver($event)"
+    v-on:drop="onDrop($event)"
+  >
+    drop zone
+  </div>
+</div>
 </template>
 
 <script>
@@ -8,14 +27,66 @@ export default {
   name: 'Testing',
   props: {
     msg: String
+  },
+  methods: {
+    onDragStart(event) {
+
+    event
+    .dataTransfer
+    .setData('text/plain', event.target.id);
+
+    event
+    .currentTarget
+    .style
+    .backgroundColor = 'yellow';
+    },
+    onDragOver(event) {
+      event.preventDefault();
+    },
+    onDrop(event) {
+      event.preventDefault();
+      const id = event
+    .dataTransfer
+    .getData('text');
+
+    const draggableElement = document.getElementById(id);
+
+    const dropzone = event.target;
+
+    dropzone.appendChild(draggableElement);
+
+    event.dataTransfer.clearData();
+    }
   }
 }
 </script>
 
 <style scoped>
-h2 {
-  color:#f00;
-  padding: 1em;
-  text-transform: uppercase;
+.container {
+  border: 2px solid #000;
+  background-color:transparent;
+  color: #000;
+  display: flex;
+}
+
+.start-box {
+  flex-basis: 100%;
+  flex-grow: 1;
+  padding: 0.2em;
+}
+
+.reorder {
+  background-color: #ff0;
+  font-weight: normal;
+  margin-bottom: 10px;
+  margin-top: 10px;
+  padding: 10px;
+}
+
+.dropzone {
+  background-color: #cff;
+  flex-basis: 100%;
+  flex-grow: 1;
+  padding: 0.2em;
 }
 </style>
