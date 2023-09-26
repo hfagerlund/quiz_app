d541e14df8037614a94fdd22a5a8190620c0c84b
Heini Fagerlund <heini@bytesofdesign.com>
2023-09-26 17:45:12 -0400

    Customize default theme
    * Modify default (Tailwind CSS) theme customizations for legibility (as per documentation for Tailwind CSS v3.3.3 at https://tailwindcss.com/docs/theme)
    * to be revisited later

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  //prefix: 'tw-', //REMOVE this
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    //"./resources/**/*.{blade.php,js,jsx,ts,tsx,vue}",
   ],
   theme: {
+  /*
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    */
     extend: {},
   },
   plugins: [],
