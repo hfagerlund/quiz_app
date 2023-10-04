ab7e9357f87435104944ce9d6df830d4fa7dd905
Heini Fagerlund <heini@bytesofdesign.com>
2023-10-04 15:57:14 -0400

    Add question components
    * For different question 'types'

--- resources/components/App.vue
+++ resources/components/App.vue
@@ -7,6 +7,10 @@
     </a>
 
   <button @click="component ='QuestionOne'" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Question One</button>
+
+  <button @click="component ='QuestionTwo'" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Question Two</button>
+
+  <button @click="component ='QuestionThree'" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Question Three</button>
   <p>*************************************</p>
   <component :is="component" />
   </div>
@@ -14,13 +18,15 @@
 </template>
 
 <script>
-import QuestionsList from '../../resources/components/QuestionsList.vue';
+import QuestionsList from './QuestionsList.vue';
 import QuestionOne from './QuestionOne.vue';
+import QuestionTwo from './QuestionTwo.vue';
+import QuestionThree from './QuestionThree.vue';
 
   export default {
     name: 'app',
     components: {
-      QuestionsList, QuestionOne
+      QuestionsList, QuestionOne, QuestionTwo, QuestionThree
     },
   data() {
     return {
---
+++ resources/components/QuestionThree.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>Question Three</h2>
+  <p>Multiple choice:</p>
+</template>
+
+<script>
+export default {
+  name: 'QuestionThree',
+  }
+</script>
+
+<style scoped>
+</style>
