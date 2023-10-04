b7fdc557d5d3202638678e2d9493a37ad01a4594
Heini Fagerlund <heini@bytesofdesign.com>
2023-10-04 18:35:02 -0400

    Query an individual question in component
    * Load API data into question 2

--- resources/components/QuestionTwo.vue
+++ resources/components/QuestionTwo.vue
@@ -1,12 +1,42 @@
 <template>
-  <h2>Question Two</h2>
-  <p>Fill in the blank:</p>
+  <h2>Question Two - {{ quizDataList.title }}</h2>
+  <div class="post">
+    <p>Fill in the blank:</p>
+    {{ quizDataList.body }}
+  </div><!-- END .post -->
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'QuestionTwo',
-  }
+        name: 'QuestionTwo',
+        data() {
+            return {
+                dataReady: false,
+                quizDataList: []
+            };
+        },
+        async mounted() {
+            try {
+                var result = await axios({
+                    method: "POST",
+                    url: "http://0.0.0.0/graphql",
+                    data: {
+                        query: `query {
+              getQuestion(id:2) {
+                title
+                body
+              }
+            }`
+                    }
+                });
+                this.dataReady = true;
+                this.quizDataList = result.data.data.getQuestion;
+            } catch (error) {
+                //console.error(error);
+            }
+        }
+    }
 </script>
 
 <style scoped>
